8aa9c1b6620770da957b2b67eb590018fc6c4c0c
George Stone <george@westborn.com.au>
2024-11-26 12:50:16 +1100

    fix postcss processing , add some tailwind classes to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+<p class="text-primary-500">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
