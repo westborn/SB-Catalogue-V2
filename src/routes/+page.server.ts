d7657c4569cf43d7ad409ef8ed5ce8c537bdd161
George Stone <george@westborn.com.au>
2024-11-30 18:46:46 +1100

    refactor: Remove debug logging from page server load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,12 +4,10 @@ import { PUBLIC_MAX_CATALOGUE_YEAR } from '$env/static/public';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	console.log(`${event.route.id} - LOAD - START`);
-	console.log(event.url.searchParams);
+	// console.log(`${event.route.id} - LOAD - START`);
 	const entryYear = event.url.searchParams.get('year') ?? '2025';
 	// Only admins can see the current exhibition year entries
 	if (entryYear.localeCompare(PUBLIC_MAX_CATALOGUE_YEAR) > 0) {
-		console.log('entry year', entryYear, PUBLIC_MAX_CATALOGUE_YEAR);
 		return { exhibits: [] };
 	}
 	try {
